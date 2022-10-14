7eab607ba11ed62fc2150293d22845385df73b15
Shirly-Tee <taiwomary108@gmail.com>
2022-10-14 14:14:29 -0700

    alx-low-level-programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
